2aa1159a38f2f89bb23f49351349011de63add5b
Amanda <amandagurns@hotmail.ca>
2024-11-30 14:25:52 -0500

    Adjusting render link

--- src/components/constants/ApiUrls.js
+++ src/components/constants/ApiUrls.js
@@ -1,8 +1,12 @@
 const API_URLS = {
-  LOGIN: "/api/v1/user/login",
-  SIGNUP: "/api/v1/user/signup",
-  EMPLOYEE: "/api/v1/emp/employees",
-  EMPLOYEE_SEARCH: "/api/v1/emp/employees/search/",
+  LOGIN:
+    "https://one01443253-comp3123-assignment1.onrender.com/api/v1/user/login",
+  SIGNUP:
+    "https://one01443253-comp3123-assignment1.onrender.com/api/v1/user/signup",
+  EMPLOYEE:
+    "https://one01443253-comp3123-assignment1.onrender.com/api/v1/emp/employees",
+  EMPLOYEE_SEARCH:
+    "https://one01443253-comp3123-assignment1.onrender.com/api/v1/emp/employees/search/",
 };
 
 export default API_URLS;
